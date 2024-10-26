aa583fdba6bac26d3b63e3de99e3f5b06e45225f
pubudhi <pywitharana@gmail.com>
2024-10-26 14:43:10 +0530

    updated customer controller

--- services/customer-service/src/main/java/com/ecommerce/customer/controller/CustomerController.java
+++ services/customer-service/src/main/java/com/ecommerce/customer/controller/CustomerController.java
@@ -3,12 +3,19 @@ package com.ecommerce.customer.controller;
 import com.ecommerce.customer.response.CustomerResponse;
 import com.ecommerce.customer.service.CustomerService;
 import com.ecommerce.customer.validation.CustomerRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.netflix.eureka.http.RestTemplateTransportClientFactories;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -32,7 +39,7 @@ public class CustomerController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CustomerResponse>> findAllCustomers(ServletResponse servletResponse) {
+    public ResponseEntity<List<CustomerResponse>> findAllCustomers() {
         return ResponseEntity.ok(customerService.findAllCustomers());
     }
 
